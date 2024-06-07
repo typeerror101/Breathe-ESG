3128c0bf22ca7d8d84ca6ca2d01b08efabb79374
Larosh Benny <57269077+typeerror101@users.noreply.github.com>
2024-06-07 09:34:36 +0530

    update

--- BreatheESG/src/contexts/authContext/index.jsx
+++ BreatheESG/src/contexts/authContext/index.jsx
@@ -1,55 +1,55 @@
-import React, { useContext, useEffect, useState } from "react";
-import  {auth} from "../firebase/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+// import React, { useContext, useEffect, useState } from "react";
+// import  {auth} from "../firebase/firebase";
+// import { onAuthStateChanged } from "firebase/auth";
 
 
-const AuthContext = React.createContext();
+// const AuthContext = React.createContext();
 
-export function useAuth() {
-    return useContext(AuthContext);
-}
-export function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [userLoggedIn, setUserLoggedIn] = useState(false);
-    const [loading, setLoading] = useState(true);
+// export function useAuth() {
+//     return useContext(AuthContext);
+// }
+// export function AuthProvider({ children }) {
+//     const [currentUser, setCurrentUser] = useState(null);
+//     const [userLoggedIn, setUserLoggedIn] = useState(false);
+//     const [loading, setLoading] = useState(true);
 
-    // function signup(email, password) {
-    //     return auth.createUserWithEmailAndPassword(email, password);
-    // }
+//     // function signup(email, password) {
+//     //     return auth.createUserWithEmailAndPassword(email, password);
+//     // }
 
-    // function login(email, password) {
-    //     return auth.signInWithEmailAndPassword(email, password);
-    // }
+//     // function login(email, password) {
+//     //     return auth.signInWithEmailAndPassword(email, password);
+//     // }
 
-    // function logout() {
-    //     return auth.signOut();
-    // }
+//     // function logout() {
+//     //     return auth.signOut();
+//     // }
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, intializeUser);
-        return unsubscribe;
-    }, [])
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, intializeUser);
+//         return unsubscribe;
+//     }, [])
 
-    async function intializeUser(user) {
-        if (user) {
-            setCurrentUser(user);
-            setUserLoggedIn(true);
-        } else {
-            setCurrentUser(null);
-            setUserLoggedIn(false);
-        }
-        setLoading(false);
-    }
+//     async function intializeUser(user) {
+//         if (user) {
+//             setCurrentUser(user);
+//             setUserLoggedIn(true);
+//         } else {
+//             setCurrentUser(null);
+//             setUserLoggedIn(false);
+//         }
+//         setLoading(false);
+//     }
 
-    const value = {
-        currentUser,
-        userLoggedIn,
-        loading
-    }
+//     const value = {
+//         currentUser,
+//         userLoggedIn,
+//         loading
+//     }
 
-    return (
-        <AuthContext.Provider value={value}>
-            {!loading && children}
-        </AuthContext.Provider>
-    )
-}
+//     return (
+//         <AuthContext.Provider value={value}>
+//             {!loading && children}
+//         </AuthContext.Provider>
+//     )
+// }
